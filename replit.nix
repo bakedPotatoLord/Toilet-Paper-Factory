68a10a5a66580a14a62e77b9d847885df98c9ffd
Josiah Hamm <93680829+bakedPotatoLord@users.noreply.github.com>
2022-11-24 03:11:13 +0000

    add typecsript

---
+++ replit.nix
@@ -0,0 +1,9 @@
+{ pkgs }: {
+  deps = [pkgs.nodejs-16_x
+pkgs.nodejs-16_x
+    pkgs.nodejs-16_x
+		pkgs.nodePackages.typescript
+    pkgs.nodePackages.vscode-langservers-extracted
+    pkgs.nodePackages.typescript-language-server
+  ];
+}
